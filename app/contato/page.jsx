285a9318f9aafe196ef5791dbfff3d850887980f
kevensouzz <souzadossantoskeven@gmail.com>
2023-11-13 20:46:35 -0300

    contact page finished

--- app/contato/page.jsx
+++ app/contato/page.jsx
@@ -35,13 +35,13 @@ export default function Contact() {
   return (
     <main className={`h-full`}>
       <div className={`container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full`}>
-        <div className={`flex flex-col w-full max-w-[700px]`}>
-          <h2 className={`font-semibold text-3xl text-center mb-12`}>
+        <div className={`flex flex-col w-full max-w-[700px] max-lg:max-w-[500px] gap-3`}>
+          <h2 className={`font-semibold text-3xl text-center max-[375px]:text-xl`}>
             Vamos <span className="text-accent">Nos Conectar!</span>
           </h2>
 
-          <form method="POST" onSubmit={sendEmail} className={`flex-1 flex flex-col gap-6 w-full mx-auto z-10`}>
-            <div className="flex gap-x-6 w-full">
+          <form method="POST" onSubmit={sendEmail} className={`flex-1 flex flex-col gap-6 w-full mx-auto z-10 max-[320px]:gap-3`}>
+            <div className="flex gap-6 w-full max-[320px]:gap-3">
               <input type="text" placeholder="Nome" aria-label="Nome" required className="input" maxLength={40} onChange={(e) => setName(e.target.value)} value={name} />
               <input type="email" placeholder="Email" aria-label="Email" required className="input" maxLength={56} onChange={(e) => setEmail(e.target.value)} value={email} />
             </div>
@@ -51,7 +51,7 @@ export default function Contact() {
             <button className="max-md:cursor-default btn rounded-full border max-md:border-accent border-white/50 max-w-[175px] max-md:max-w-[50px] transition-all duration-300 flex item-center justify-center overflow-hidden focus-visible:outline-0 md:focus-visible:border-accent md:hover:border-accent group">
               <span className="hidden md:flex translate-y-[50%] md:group-focus-visible:-translate-y-[100%] md:group-hover:-translate-y-[100%] md:group-focus-visible:opacity-0 md:group-hover:opacity-0 transition-all duration-500 ">Enviar Mensagem!</span>
 
-              <BsArrowRight className="md:-translate-y-[100%] max-md:translate-y-[50%] md:opacity-0 md:group-focus-visible:flex md:group-hover:flex md:group-focus-visible:translate-y-[50%] md:group-hover:translate-y-[50%] md:group-focus-visible:opacity-100 md:group-hover:opacity-100 transition-all duration-500 absolute text-[22px] text-accent" />
+              <BsArrowRight className="md:-translate-y-[100%] max-md:translate-y-[60%] md:opacity-0 md:group-focus-visible:flex md:group-hover:flex md:group-focus-visible:translate-y-[50%] md:group-hover:translate-y-[50%] md:group-focus-visible:opacity-100 md:group-hover:opacity-100 transition-all duration-500 absolute text-[22px] text-accent" />
             </button>
           </form>
         </div>
--- components/Modal.jsx
+++ components/Modal.jsx
@@ -4,10 +4,10 @@ export default function Modal({ onClose, message }) {
   return (
     <section className={`fixed z-50 inset-0 bg-black text-accent bg-opacity-75 backdrop-blur-[2px] flex flex-col justify-center items-center`}>
       <div
-        className={`w-[450px] h-[125px] overflow-hidden flex flex-col rounded-2xl border-t-4 border-accent bg-opacity-75 max-sm:w-[375px] max-sm:h-[100px] max-[400px]:w-[275px]`}
+        className={`w-[450px] h-[125px] overflow-hidden flex flex-col rounded-2xl border-t-4 border-accent bg-opacity-75 max-sm:w-[375px] max-sm:h-[100px] max-[400px]:w-[275px] max-[320px]:w-[225px]`}
       >
         <span
-          className={`w-full h-3/5 flex items-center justify-center font-semibold text-2xl max-sm:text-xl max-[400px]:text-lg`}
+          className={`w-full h-3/5 flex items-center justify-center font-semibold text-2xl max-sm:text-xl max-[400px]:text-lg max-[320px]:text-base`}
         >
           {message}
         </span>
