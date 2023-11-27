6d28338f2a90023ad5210dfa24ab7e92dcf9fd58
kevensouzz <souzadossantoskeven@gmail.com>
2023-11-27 10:40:45 -0300

    optimizing images

--- components/BackRightImg.jsx
+++ components/BackRightImg.jsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
-import backright from "@/public/back-right-img.png"
 
 export default function BackRightImg() {
   return (
     <div className={`absolute right-0 bottom-0 mix-blend-color-dodge animate-pulse duration-300 ease-linear`}>
       <Image
-        src={backright}
+        src={"/back-right-img.png"}
         alt=""
         draggable="false"
+        width={450}
+        height={273}
       />
     </div>
   )
--- components/TopLeftImg.jsx
+++ components/TopLeftImg.jsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
-import topleft from "@/public/top-left-img.png"
 
 export default function TopLeftImg() {
   return (
     <div className={`absolute left-0 top-0 mix-blend-color-dodge opacity-25`}>
       <Image
-        src={topleft}
+        src={"/top-left-img.png"}
         alt=""
         draggable="false"
+        width={411}
+        height={405}
       />
     </div>
-  )
+  )   
 }
