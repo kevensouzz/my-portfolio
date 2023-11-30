0cee9a72c45b27017cf14557f7260efdabcede32
kevensouzz <souzadossantoskeven@gmail.com>
2023-11-29 21:16:45 -0300

    add cv to download

--- components/HomeButtons.jsx
+++ components/HomeButtons.jsx
@@ -8,7 +8,7 @@ import { fadeIn } from '@/variants';
 export default function HomeButtons() {
   return (
     <motion.div variants={fadeIn("up", 0.3)} initial="hidden" animate="show" exit="hidden" className={`w-full max-w-[60%] max-md:max-w-[100%] max-md:h-[92.5px] flex items-center justify-between max-md:flex-col`}>
-      <a href={"/myCurriculum.pdf"} download={"kevensouza.pdf"} className={`border border-white rounded-full cursor-pointer max-md:cursor-default w-full py-2 max-sm:py-[6px] max-w-[175px] max-[320px]:max-w-[150px] transition-all duration-300 ease-linear flex gap-1 md:gap-2 item-center justify-center overflow-hidden focus-visible:outline-0 md:focus-visible:border-accent hover:border-accent md:focus-visible:bg-accent hover:bg-accent`}>
+      <a href={"/kevensouza.pdf"} download={"kevensouza.pdf"} className={`border border-white rounded-full cursor-pointer max-md:cursor-default w-full py-2 max-sm:py-[6px] max-w-[175px] max-[320px]:max-w-[150px] transition-all duration-300 ease-linear flex gap-1 md:gap-2 item-center justify-center overflow-hidden focus-visible:outline-0 md:focus-visible:border-accent hover:border-accent md:focus-visible:bg-accent hover:bg-accent`}>
         <p>Baixar CV</p>
         <GoDownload className={`mt-[2px] text-lg`} />
       </a>
